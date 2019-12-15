0f598628354edcd12f3e4e9af191f071817d244d
tthanh <tranthuanthanh@users.noreply.github.com>
2019-12-15 12:27:15 +0700

    Search and view all

--- models/book.model.js
+++ models/book.model.js
@@ -3,5 +3,5 @@ const config = require('../configs/bookModelConfig.json');
 
 module.exports = {
     all: _ => db.load('select * from book'),
-    search: (keyword) => db.load("select * from book where match (title) against('${keyword}')"),
+    search: (keyword) => db.load("select * from book where match (title) against('" + keyword + "')"),
 };
